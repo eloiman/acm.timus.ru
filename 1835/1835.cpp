23f043b01e4d3bf48567bdb19f3c275b85aeed5c
Sergey Strelnikov <eloimann@gmail.com>
2012-08-02 22:35:26 +0400

    1835 : lpath

--- 1835/1835.cpp
+++ 1835/1835.cpp
@@ -6,6 +6,8 @@
 #include <errno.h>
 #include <err.h>
 #include <vector>
+#include <map>
+#include <utility>
 #include <ctype.h>
 
 #ifndef ONLINE_JUDGE
@@ -14,21 +16,16 @@
 #define TRACE(...)  ;
 #endif
 
-#define ALLOC_SIZE	(32767)
+#define ALLOC_SIZE	        (32767)
+#define NLETTERS            (('z' - 'a' + 1) + 1)
+#define LETTER_IDX(L)       (('.' == L) ? 0 : (L - 'a' + 1))
+#define MAKE_LPATH(L)       (LETTER_IDX(L) << NLETTERS)
+#define SET_LPATH(L, P)     (P = P | LETTER_IDX(L))
+#define TEST_LPATH(P, L)    (P = P & ~(1 << LETTER_IDX(L)))
+#define SET_LPATH_IDX(LI, P)     (P = P | LI)
+#define TEST_LPATH_IDX(P, LI)    (P = P & ~(1 << LI))
 
 using namespace std;
-
-
-struct STextBlock {
-    int ninput_begin;
-    char *pbegin;
-    int ninput_end;
-    char *pend;
-    char *pat;
-};
-
-typedef list<STextBlock *>              text_blocks;
-typedef text_blocks::iterator           text_blocks_iter;
 
 struct SInputBlock {
     char *pinput;
@@ -38,58 +35,112 @@ struct SInputBlock {
 typedef vector<SInputBlock *>           input_blocks;
 typedef input_blocks::iterator          input_blocks_iter;
 
+typedef unsigned char                   letter;
+typedef unsigned long                   epos;
+typedef unsigned long                   lpath;
+
+typedef lpath *                         arr_lpath;
+typedef map<epos, arr_lpath>            map_pos_letters;
+typedef pair<epos, arr_lpath>           pair_pos_letters;
+typedef lpath *                         arr_ats;
+
 struct SState {
-    input_blocks    input;
-    text_blocks     email_blks;
+    input_blocks        input;
+    map_pos_letters     emails_user_graph;
+    map_pos_letters     emails_domain_graph;
+    arr_ats             emails_at_graph;
 };
 
 static SState s_state;
 
-int parse_input()
+typedef enum {
+    ISUSER,
+    ISAT,
+    ISDOMAIN
+} ISUAD;
+
+int add_letter(ISUAD uad, epos pos, letter prev_letter, letter new_letter) 
 {
-    unsigned long ninput = 0;
-    unsigned long len = 0;
     int ret_val = 0;
-    STextBlock *tb = NULL;
-    unsigned long i = 0;
-    char *in = NULL;
-    char ch;
-    input_blocks_iter input_iter = s_state.input.begin();
+    lpath prev_lpath = 0;
+    arr_lpath parr_lpath = NULL;
+    unsigned char prev_letter_idx = LETTER_IDX(prev_letter);
+    unsigned char letter_idx = LETTER_IDX(new_letter);
+
+    if (ISUSER == uad) {
+        map_pos_letters::iterator impl = s_state.emails_user_graph.find(pos);
+        if (impl == s_state.emails_user_graph.end()) {
+            parr_lpath = new lpath[NLETTERS];
+            fill(parr_lpath, parr_lpath + NLETTERS, 0);
+            s_state.emails_user_graph[pos] = parr_lpath;
+        } else {
+            parr_lpath = (*impl).second;
+        }
+        SET_LPATH_IDX(parr_lpath[prev_letter_idx], letter_idx);
+    } else if (ISUSER == uad) {
+        map_pos_letters::iterator impl = s_state.emails_domain_graph.find(pos);
+        if (impl == s_state.emails_domain_graph.end()) {
+            parr_lpath = new lpath[NLETTERS];
+            fill(parr_lpath, parr_lpath + NLETTERS, 0);
+            s_state.emails_user_graph[pos] = parr_lpath;
+        } else {
+            parr_lpath = (*impl).second;
+        }
+        SET_LPATH_IDX(parr_lpath[prev_letter_idx], letter_idx);
+    } else if (ISAT == uad) {
+        SET_LPATH_IDX(s_state.emails_at_graph[prev_letter_idx], letter_idx);
+    } else {
+        exit(0);
+    }
     
+    return 0;
+}
+
+
+void parse_input()
+{
+    enum {
+        USER,
+        DOMAIN,
+        USER_DOMAIN
+    } udstatus = USER;
+    enum {
+        PREV_NONE,
+        PREV_AT,
+        PREV_LETTER,
+        PREV_DOR
+    } prevstatus = PREV_NONE;
+    unsigned long inpos = 0;
+    char *in = NULL;
+    char ch = '\0';
+    char prevch = '\0';
+    unsigned char seg = 0;
+    input_blocks_iter input_iter;
+    unsigned char *puser = NULL;
+    unsigned char user_seg = 0;
+    unsigned char *pdomain = NULL;
+    unsigned char domain_seg = 0;
+    unsigned long char_pos = 0;
+        
+    seg = 0;
+    input_iter = s_state.input.begin();
     while (input_iter != s_state.input.end()) {
-        len = 0;
+        inpos = 0;
         in = (*input_iter)->pinput;
         do {
-            if (('\0' == *in || ' ' == *in)) {
-                ch = *in;
-                if (tb) {
-                    tb->ninput_end = ninput;
-                    tb->pend = in;
-                    s_state.email_blks.push_back(tb);
-                    tb = NULL;
-                    *in = '\0';
-                }
-                if ('\0' == ch) {
-                    break;
-                }
-            } else if ('.' == *in || '@' == *in || islower(*in)) {
-                if (!tb) {
-                    tb = new STextBlock();                
-                    tb->ninput_begin = ninput;
-                    tb->pbegin = in;
-                }
-                if ('@' == *in) {
-                    tb->pat = in;
+            ch = in;
+            if ('.' == ch) {
+                if (PREV_LETTER != prevstatus) {
+                    puser = in;
+                    user_seg = seg;
                 }
             }
-            len++;
+            prevch = ch;
             in++;
-        } while(len < (*input_iter)->sz);
-        ninput++;
+        } while(in < (*input_iter)->sz);
         input_iter++;
+        seg++;
     }
-
-    return ret_val;
 }
 
 int read_input()
@@ -112,11 +163,10 @@ int read_input()
         }
     } while(!ferror(stdin) && !feof(stdin));
 
-    //TRACE("feof : %d; ferror %d; errno = %d (%s)\n", feof(stdin), ferror(stdin), errno, strerror(errno));
-
     return 0;
 }
 
+/*
 void trace_blocks() 
 {
     unsigned long nsegi;
@@ -138,6 +188,7 @@ void trace_blocks()
         i++;
     }
 }
+*/
 
 int main()
 {
@@ -145,5 +196,5 @@ int main()
 
     read_input();
     parse_input();
-    trace_blocks();
+    //trace_blocks();
 }
