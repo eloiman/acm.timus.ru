05042a29ff4aec578d3ae88c0c2113c55ed55ee2
user <user@atlinpc.(none)>
2012-10-03 14:10:12 +0400

    1402 and big numbers

--- 1402/1402.cpp
+++ 1402/1402.cpp
@@ -2,155 +2,203 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <vector>
+
+#define BIGN_SZ     (100)
 
 typedef long long llong;
+typedef unsigned long long ullong;
+typedef struct {
+    char * arr;
+    int sz;
+} bign;
 
-char * alloc_bign(int sz)
+bign bign_alloc(int sz = BIGN_SZ)
 {
-    return (char *) calloc(sz, 1);
-}
-
-void free_bign(char *bign)
-{
-    free(bign);
-}
-
-void assign_bign(char *bign, int sz, llong n)
-{
-    int intgr;
-    int rem;
-    int i = 0;
-    
-    do {
-        rem = n % 10;
-        intgr = n / 10;
-        n = intgr;
-
-        bign[i++] = rem;
-    } while (intgr);
-}
-
-char * copy_bign(char *bign, int sz)
-{
-    char *n = alloc_bign(sz);
-    
-    memcpy(n, bign, sz);
+    bign n;
+    n.arr = static_cast<char*>(calloc(sz, 1));
+    n.sz = sz;
 
     return n;
 }
 
-
-char * summ_bign(char *a, char *b, int sz)
+void bign_free(bign n)
 {
-    char *summ = alloc_bign(sz);
+    free(n.arr);
+}
+
+void bign_assign(bign n, ullong val)
+{
+    int i;
+    int r;
+    int k = 0;
+   
+    memset(n.arr, 0, n.sz);
+ 
+    do {
+        r = val % 10;
+        i = val / 10;
+        val = i;
+
+        n.arr[k++] = r;
+    } while (i);
+}
+
+bign bign_dup(bign n)
+{
+    bign m = bign_alloc(n.sz);
+    
+    memcpy(m.arr, n.arr, n.sz);
+
+    return m;
+}
+
+
+bign operator +(bign n, bign m)
+{
+    bign summ = bign_alloc(n.sz);
     int tmp;
+    int sz = n.sz;
 
     for (int i = 0; i < sz; i++) {
-        tmp = a[i] + b[i];
-        summ[i] += (tmp % 10);
-        if (tmp > 9 && i != (sz - 1)) {
-            summ[i + 1] += 1;
+        tmp = n.arr[i] + m.arr[i];
+        summ.arr[i] += tmp;
+        if (summ.arr[i] > 9 && i != (sz - 1)) {
+            summ.arr[i] -= 10;
+            summ.arr[i + 1] += 1;
         }
     }
 
     return summ;
 }
 
-char *mul_bign(char *a, int sz, int n)
+bign operator *(bign n, bign m)
 {
-    char * summ_prev = copy_bign(a, sz);
-    char * summ = NULL;
-    char *b = copy_bign(a, sz);
-    int i = 1;
+    std::vector<bign> muls;
+    std::vector<bign>::iterator muls_iter;
+    bign mul = bign_alloc(n.sz);
+    bign mul0;
+    int tmp;
+    int ii;
+    int rr;
+    int kk = 0;
+    int pos = 0;
+    int tup = 0;
+     
+    for (int i = 0; i < m.sz; i++) {
+        bign t = bign_alloc();
+        tup = 0;
+        for (int j = 0; j < n.sz; j++) {
+            tmp = m.arr[i] * n.arr[j];
+            if (tmp) {
+                kk = i + j;
+                do {
+                    rr = tmp % 10;
+                    ii = tmp / 10;
+                    tmp = ii;
+                    t.arr[kk] += rr;
+                    if (t.arr[kk] > 9) {
+                        t.arr[kk] -= 10;
+                        t.arr[kk + 1] += 1;
+                    }
+                    kk++;
+                } while (ii);
+                tup = 1;
+            }
+        }
+        if (tup) {
+            muls.push_back(t);
+        } else {
+            bign_free(t);
+        }
+    }
 
-    do {
-        summ = summ_bign(summ_prev, b, sz);
-        free_bign(summ_prev);
-        summ_prev = summ;
-    } while(++i < n);
-   
-    return summ;
+    mul0 = mul;
+    for (muls_iter = muls.begin(); muls_iter != muls.end(); muls_iter++) {
+        bign t = *muls_iter;
+        mul = mul0 + t;
+        bign_free(mul0);
+        mul0 = mul;
+        bign_free(t);
+    }
+
+    return mul;
 }
 
-void print_bign(char *bign, int sz)
+void bign_print(bign n)
 {
     int a = 0;
 
-    for (int i = sz - 1; i >= 0; i--) {
-        if (bign[i] || a) {
-            printf("%d", bign[i]);
+    for (int i = n.sz - 1; i >= 0; i--) {
+        if (n.arr[i] || a) {
+            printf("%d", n.arr[i]);
             a = 1;
         }
     }
+
+    if (!a) {
+        printf("0");
+    }
 }
 
-double calc_a_n_m(int n, int m)
+bign calc_a_n_m(int n, int m)
 {
     int en = n - m + 1;
     int ni = n;
-    double res = (double) n;
+    bign res = bign_alloc();
+    bign res0;
+    bign tmp = bign_alloc();
 
+    bign_assign(res, n);
+
+    res0 = res;
     do {
         ni -= 1;
-        res *= double(ni);
+        bign_assign(tmp, ni);
+        res = res0 * tmp;
+        bign_free(res0);
+        res0 = res;        
     } while (ni != en);
 
     return res;
 }
 
-double resolve(int n)
+bign resolve(int n)
 {
-    double res = 0.;
+    bign res = bign_alloc();
+    bign res0;
+    bign anm;
+
+    bign_assign(res, 0);
 
     if (n == 1) {
-        return 0.;
+        return res;
     }
 
+    res0 = res;
     for (int m = 2; m <= n; m++) {
-        res += calc_a_n_m(n, m);
+        anm = calc_a_n_m(n, m);
+        res = res0 + anm;
+        bign_free(res0);
+        bign_free(anm);
+        res0 = res;
     }
 
     return res;
 }
 
-void out_double(double d)
-{
-    double fraq;
-    double integral;
-
-    if (d < 10) {
-        printf("%d", int(d + 0.5));
-        return;
-    }
-        
-    fraq = modf(d / 10., &integral);
-    out_double(integral);
-
-    printf("%d", int((fraq + 0.5) * 10.));
-}
-
 int main(void)
 {
     int N = 0;
-    double res = 0.;
-    double integral;
+    bign res;
 
-    //scanf("%d", &N);
+    scanf("%d", &N);
     
-    char *a = alloc_bign(100);
-    assign_bign(a, 100, 21000);
-    char *c0 = mul_bign(a, 100, 21001);
-    char *c1 = mul_bign(c0, 100, 21000);
-    char *c2 = mul_bign(c1, 100, 21000);
-    char *c3 = mul_bign(c2, 100, 21000);
+    res = resolve(N);
     
-    print_bign(c3, 100);
+    bign_print(res);
 
-    //res = resolve(N);
-
-    //modf(res, &integral);
-    
-    //out_double(integral);
+    bign_free(res);
 
     return 0;
 }
